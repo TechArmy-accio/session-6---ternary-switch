c2ee4d223d8d8d2c4a60b31a9f3c74b5e3bf5921
Instructor <dodydev311@gmail.com>
2024-06-16 13:55:42 +0530

    session 6 - ternory

---
+++ switch.js
@@ -0,0 +1,17 @@
+
+// "red" => "Stop"
+// "yellow" => "Slow down"
+// "green" => "po"
+
+let color = "red";
+
+
+switch(choice){
+
+    case choice1: 
+
+    case choice2:
+
+    case choice3:
+
+} 
---
+++ ternary.js
@@ -0,0 +1,75 @@
+// Take two distinct number and find the buggest of two
+
+// if(a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+
+
+// ternoary: 
+
+//  10>20 ?  console.log("P") : console.log("Q")
+
+// let a = 100 
+// let b = 200
+  
+//  a>b ?  console.log("a is bigger") : console.log("b is bigger")
+
+
+// let n = 561
+
+//  n%2==0 ? console.log("Even") : console.log("Odd")
+
+// console.log(  n%2==0? "Even" : "Odd" )
+
+
+// lets take a number anc heck it is positive or not
+
+// postive, negative, zero
+// let n = 0
+
+//  n>0? console.log("Positive") : 
+//                              (n==0 ? console.log("zero") :  console.log("Negative") )
+
+
+// a,b,c => biggest of 3
+
+// let a = 1000 
+// let b = 2000
+// let c = 15000
+
+//  a>b&& a>c ? 
+//             console.log("a is bigger") : 
+//                                          b>c && b>a ? console.log("b is bigger"): console.log("c is bigger")
+
+
+
+// 1) lets take a 3 digit number and check its middle digits is div by 8 or not
+
+
+// let n = 567 
+// let firstTwoDigit = parseInt(n/10) // 56 
+// let middle = firstTwoDigit%10 // 6
+
+// middle%8==0 ? console.log("Div by 8") : console.log("Not Div by 8")
+
+
+// 2) Take a number and check it is a one digit number, 2 digit or 3 digit number or more than 3 digit.
+
+// let n = 51645
+
+
+// console.log(n<=9 ? "one digit" : 
+//                     n<=99 ? "two digit" : 
+//                                          n<=999 ? "three digit" : "more than 3 digit")
+                    
+
+
+// Take two numbers and one operator as input(out of arithmetic operator) and print the output based on user choice.
+// Eg:  10, 20, + =>  10+20 = 30
+
+// let a = 10
+// let b = 20
+// let operator = "*" // +, -, *, /
